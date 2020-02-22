0dbc7b3d852e2472a172df6ef1e6f3c9ce26d37c
Thanos Zouganelis <thanos.zouganelis@hiziggy.com>
2020-02-22 14:38:52 +0200

    Added Unit Testing Base Implementations and Shared Project

---
+++ CalculatorAppTests/CalculatorTests/HelloWorldTests.cs
@@ -0,0 +1,129 @@
+﻿using Android.Support.V7.App;
+using Android.Widget;
+using CalculatorApp.Activities;
+using CalculatorApp.Helpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorAppTests.CalculatorTests
+{
+    [TestFixture]
+    class HelloWorldTests
+    {
+
+        [Test]
+        public void NoFirstDigitTest()
+        {
+            CalculatorHelper calcHelper = new CalculatorHelper();
+            var calcText = calcHelper.Calculation("-");
+            Assert.That(calcText == "0 - ");
+
+            calcText = calcHelper.Calculation("*");
+            Assert.That(calcText == "0 * ");
+
+            calcText = calcHelper.Calculation("+");
+            Assert.That(calcText == "0 + ");
+
+            calcText = calcHelper.Calculation("÷");
+            Assert.That(calcText == "0 ÷ ");
+        }
+
+        [Test]
+        public void DigitsTest()
+        {
+            CalculatorHelper calcHelper = new CalculatorHelper();
+            var calcText = calcHelper.Calculation("1");
+            Assert.That(calcText == "1  ");
+
+            calcText = calcHelper.Calculation("+");
+            Assert.That(calcText == "1 + ");
+
+            calcText = calcHelper.Calculation("2");
+            Assert.That(calcText == "1 + 2");
+        }
+
+        [Test]
+        public void AddTest()
+        {
+            CalculatorHelper calcHelper = new CalculatorHelper();
+            var calcText = calcHelper.Calculation("1");
+            Assert.That(calcText == "1  ");
+
+            calcText = calcHelper.Calculation("+");
+            Assert.That(calcText == "1 + ");
+
+            calcText = calcHelper.Calculation("2");
+            Assert.That(calcText == "1 + 2");
+
+            calcText = calcHelper.Calculation("=");
+            Assert.That(calcText == "3  ");
+        }
+
+        [Test]
+        public void SubtractTest()
+        {
+            CalculatorHelper calcHelper = new CalculatorHelper();
+            var calcText = calcHelper.Calculation("1");
+            Assert.That(calcText == "1  ");
+
+            calcText = calcHelper.Calculation("-");
+            Assert.That(calcText == "1 - ");
+
+            calcText = calcHelper.Calculation("2");
+            Assert.That(calcText == "1 - 2");
+
+            calcText = calcHelper.Calculation("=");
+            Assert.That(calcText == "-1  ");
+        }
+
+        [Test]
+        public void DevideTest()
+        {
+            CalculatorHelper calcHelper = new CalculatorHelper();
+            var calcText = calcHelper.Calculation("1");
+            Assert.That(calcText == "1  ");
+
+            calcText = calcHelper.Calculation("÷");
+            Assert.That(calcText == "1 ÷ ");
+
+            calcText = calcHelper.Calculation("2");
+            Assert.That(calcText == "1 ÷ 2");
+
+            calcText = calcHelper.Calculation("=");
+            Assert.That(calcText == "0.5  ");
+        }
+
+        [Test]
+        public void MultiplyTest()
+        {
+            CalculatorHelper calcHelper = new CalculatorHelper();
+            var calcText = calcHelper.Calculation("1");
+            Assert.That(calcText == "1  ");
+
+            calcText = calcHelper.Calculation("*");
+            Assert.That(calcText == "1 * ");
+
+            calcText = calcHelper.Calculation("2");
+            Assert.That(calcText == "1 * 2");
+
+            calcText = calcHelper.Calculation("=");
+            Assert.That(calcText == "2  ");
+        }
+
+        [Test]
+        public void DotTest()
+        {
+            CalculatorHelper calcHelper = new CalculatorHelper();
+            var calcText = calcHelper.Calculation("1");
+            Assert.That(calcText == "1  ");
+
+            calcText = calcHelper.Calculation(".");
+            Assert.That(calcText == "1.  ");
+
+            calcText = calcHelper.Calculation(".");
+            Assert.That(calcText == "1.  ");
+        }
+    }
+}
